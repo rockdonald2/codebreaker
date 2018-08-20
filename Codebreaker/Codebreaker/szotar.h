77014312620918b56b933b0b0148a1c3e9ff3a9f
Lukács Zsolt <39481074+rockdonald2@users.noreply.github.com>
2018-08-20 20:20:30 +0300

    2018\08\20
    Kezdet

---
+++ Codebreaker/Codebreaker/szotar.h
@@ -0,0 +1,18 @@
+#ifndef SZOTAR_H
+#define SZOTAR_H
+
+// a szotar fajl, amiben a jatek altal hasznalt szavak talalhatok meg
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+namespace Szotar
+{
+	vector<string> szotar{ "AHEM", "AHIS", "AHOY", "AIDS", "AIMS", "AIRS", "ALOE", "ALSO",
+		};
+}
+
+#endif
