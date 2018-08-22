6cb6fd9d7c515f3b36caf31bca3f22a9241f77e5
Lukács Zsolt <39481074+rockdonald2@users.noreply.github.com>
2018-08-22 22:29:00 +0300

    20180822

---
+++ Codebreaker/Codebreaker/absztrakt.h
@@ -0,0 +1,15 @@
+#ifndef ABSZTRAKT_H
+#define ABSZTRAKT_H
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+namespace Absztrakt
+{
+	vector<string> absztrakt{ "?", "!", "\"", "£", "$", "%", "&", "*", "(", ")", "=", "+", "#" };
+}
+
+#endif
--- Codebreaker/Codebreaker/szotar.h
+++ Codebreaker/Codebreaker/szotar.h
@@ -12,7 +12,23 @@ using std::string;
 namespace Szotar
 {
 	vector<string> szotar{ "AHEM", "AHIS", "AHOY", "AIDS", "AIMS", "AIRS", "ALOE", "ALSO",
-		};
+		"BYTE", "CABA", "CABS", "CACA", "DAAL", "DABS", "DACE", "DACK",
+		"HIPT", "HIRE", "HISH", "HOAS", "HOAX", "HOBO", "ITAS", "ITCH", "ITEM",
+		"VOWS", "VRIL", "VROT", "VROU",
+		"SAAG", "SABE", "SABS", "SACK", "SCAB", "SCAD", "SCAG", "SCAM",
+		"ROWS", "ROWT", "RUBE", "RUGA", "RUGS", "RUIN", "RUKH",
+		"RYES", "RYFE", "RYKE",
+		"PUMY", "PUNA", "PUNG", "PYIC", "PYIN", "PYNE",
+		"MUNG", "MUNI", "MUNS", "MUNT", "MUZZ", "MWAH", "MYAL",
+		"SAID", "SAIL", "SAIM", "SAIN", "SAIR",
+		"OWLY", "OWNS", "OWRE", "OURN", "OURS", "OUST",
+		"NABK", "NABS", "NACH", "NADA", 
+		"TELL", "TELS", "TELT", "TEME", "THEY", "THIG", "THIN",
+		"SNEE", "SNIB", "SNIG", "SNIP"
+	};
+
+	//cout << Szotar::szotar[0];
+		//cout << Szotar::szotar[0].at(1);
 }
 
 #endif
